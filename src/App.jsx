6bfae4e03776a451b023a915886f3a4a09b7c941
MAURÍCIO TELLES SILVA BICHELS <mauricio_bichels@estudante.sesisenai.org.br>
2025-07-03 08:56:21 -0300

    runing on Hopes and dreams

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Natureza from "./componentes/natureza";
+import Twd from "./componentes/Twd";
 
 export default function App() {
   return (
     <div className="app">
       <Natureza />
-      <twd />
+      <Twd />
     </div>
   );
 }
